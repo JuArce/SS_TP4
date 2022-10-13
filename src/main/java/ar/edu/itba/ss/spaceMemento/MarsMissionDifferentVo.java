b950b5bf8943954376d145f3bcb858ef6c48aa24
Julian Arce <juarce@itba.edu.ar>
2022-10-12 23:22:59 -0300

    feat: launch spaceship with different vo

---
+++ src/main/java/ar/edu/itba/ss/spaceMemento/MarsMissionDifferentVo.java
@@ -0,0 +1,56 @@
+package ar.edu.itba.ss.spaceMemento;
+
+import ar.edu.itba.ss.spaceMemento.interfaces.Exporter;
+import ar.edu.itba.ss.spaceMemento.models.CelestialBody;
+import ar.edu.itba.ss.spaceMemento.models.SolarSystem;
+import ar.edu.itba.ss.spaceMemento.utils.CelestialBodyFactory;
+import ar.edu.itba.ss.spaceMemento.utils.DistanceVelocityExporter;
+import ar.edu.itba.ss.spaceMemento.utils.OvitoExporter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
+
+public class MarsMissionDifferentVo {
+    public static void main(String[] args) {
+        List<Double> vo = List.of(7.998, 7.999, 8.0, 8.001, 8.002, 8.003, 8.004, 8.005);
+
+        final LocalDate initialDate = LocalDate.of(2022, 9, 23);
+        final LocalDateTime launchDate = LocalDateTime.of(2024, 10, 30, 0, 20, 0);
+        final int minutesOffset = launchDate.getHour() * 60 + launchDate.getMinute();
+
+        final String distanceFilename = "distance_diffVo_" + launchDate + ".csv";
+        final Exporter distanceExporter = new DistanceVelocityExporter("marsMission/output/", distanceFilename.replaceAll(":", "-"), vo);
+        distanceExporter.open();
+
+        vo.forEach(spaceshipVo -> {
+            System.out.println("******************************");
+            long startTime = System.currentTimeMillis();
+
+            final double dt = 300;
+            final double tf = 365.25 * 24 * 3600 * 3;
+
+            final CelestialBody earth = CelestialBodyFactory.getEarth();
+            final CelestialBody mars = CelestialBodyFactory.getMars();
+
+            final String ovitoFilename = "marsMissionSpecific_" + launchDate + "_vo_" + spaceshipVo + ".txt";
+            final Exporter ovitoExporter = new OvitoExporter("marsMission/output/ovito/", ovitoFilename.replaceAll(":", "-"));
+            ovitoExporter.open();
+
+            final int spaceshipLaunchDay = (int) DAYS.between(initialDate, launchDate);
+
+            final SolarSystem solarSystem = new SolarSystem(earth, mars, dt, tf, initialDate, spaceshipLaunchDay, minutesOffset, spaceshipVo, ovitoExporter, distanceExporter, null);
+
+            solarSystem.run();
+
+            ovitoExporter.close();
+
+            long endTime = System.currentTimeMillis();
+            System.out.println("That took " + (endTime - startTime) + " milliseconds");
+            System.out.println("******************************");
+        });
+        distanceExporter.close();
+    }
+}
